114d884674d46843e5b4ee4d4a296ff581fd20f4
Siddhant Kumar Patra <siddhantkumar.p@listertechnologies.com>
2017-04-20 20:03:53 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,37 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+
+
+const appRoutes: Routes = [
+  { path: 'students', component: StudentsComponent , pathMatch:'full'},
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  { path: '',
+    redirectTo: '/students',
+    pathMatch : 'full'
+   }
+  // { path: '**', component: PageNotFoundComponent }
+];
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
